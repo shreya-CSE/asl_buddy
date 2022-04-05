e1a44993655214271d75991bbc8a8a24982b4efa
omamoham <94276035+omamoham@users.noreply.github.com>
2022-04-05 01:07:49 -0500

    asl_buddy

--- app/src/main/java/com/example/asl_buddy/MainActivity.java
+++ app/src/main/java/com/example/asl_buddy/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.asl_buddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Home_button;
+    Button login_button;
+    Button sign_up_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Home_button = findViewById(R.id.button3);
+        Home_button.setOnClickListener(new View.OnClickListener()
+       {
+           @Override
+           public void onClick(View v)
+           {
+               Intent home_button_intent=new Intent(getApplicationContext(),home_page.class);
+               startActivity(home_button_intent);
+           }
+
+       }
+
+        );
+        login_button = findViewById(R.id.button);
+        login_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent login_intent=new Intent(getApplicationContext(),login_page.class);
+                startActivity(login_intent);
+            }
+
+        }
+
+        );
+
+        sign_up_button = findViewById(R.id.button2);
+        sign_up_button.setOnClickListener(new View.OnClickListener()
+          {
+              @Override
+              public void onClick(View v)
+              {
+                  Intent sign_up_intent=new Intent(getApplicationContext(),sign_up.class);
+                  startActivity(sign_up_intent);
+              }
+
+          }
+
+        );
     }
 }

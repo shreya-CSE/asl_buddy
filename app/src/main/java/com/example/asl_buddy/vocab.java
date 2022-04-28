10e62314f075bb3f85b7c87f93b532581dec92e1
Tahera <taherafatimaisi@gmail.com>
2022-04-28 13:24:28 -0500

    asl_buddy

--- app/src/main/java/com/example/asl_buddy/vocab.java
+++ app/src/main/java/com/example/asl_buddy/vocab.java
@@ -10,20 +10,48 @@ import android.widget.CheckBox;
 public class vocab extends AppCompatActivity {
 
     private CheckBox intro;
+    private CheckBox ASL;
+    private CheckBox English;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vocab);
 
-        intro=findViewById(R.id.checkBox);
+        intro = findViewById(R.id.checkBox);
         intro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intro_intent=new Intent(getApplicationContext(),intro_video.class);
+                Intent intro_intent = new Intent(getApplicationContext(), intro_video.class);
                 startActivity(intro_intent);
+            }
+        });
+
+
+        ASL = findViewById(R.id.checkBox1);
+        ASL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+
+
+        });
+
+
+        English = findViewById(R.id.checkBox2);
+        English.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
             }
         });
 
     }
 }
+
+
+
+
+
+

1718e2a9d3c67b3f58341e5a61527c05868d6c9c
Tahera <taherafatimaisi@gmail.com>
2022-05-06 00:36:47 -0500

    Completed Google SignIn (but not showing up on the Emulator)

--- app/src/main/java/com/example/asl_buddy/MainActivity.java
+++ app/src/main/java/com/example/asl_buddy/MainActivity.java
@@ -1,65 +1,91 @@
 package com.example.asl_buddy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    SignInButton signInButton;
+    private GoogleApiClient googleApiClient;
+    private static final int SIGN_IN = 1;
 
     Button login_button;
     Button sign_up_button;
-
-    //TEMP--------
-    Button skipButton;
-    //TEMP--------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TEMPORARY SKIP BUTTON
-        //--------------------------
-        skipButton = findViewById(R.id.skip);
-        skipButton.setOnClickListener(new View.OnClickListener() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, 'onConnectionFailedListener:', this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+        signInButton = findViewById(R.id.google_sign_in);
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent homepage = new Intent(getApplicationContext(), home_page.class);
-                startActivity(homepage);
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, SIGN_IN);
             }
         });
-        //-------------------------
+
 
         login_button = findViewById(R.id.button);
-        login_button.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent login_intent=new Intent(getApplicationContext(),login_page.class);
-                startActivity(login_intent);
-            }
+        login_button.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                Intent login_intent = new Intent(getApplicationContext(), login_page.class);
+                                                startActivity(login_intent);
+                                            }
 
-        }
+                                        }
 
         );
 
         sign_up_button = findViewById(R.id.button2);
-        sign_up_button.setOnClickListener(new View.OnClickListener()
-          {
-              @Override
-              public void onClick(View v)
-              {
-                  Intent sign_up_intent=new Intent(getApplicationContext(),sign_up.class);
-                  startActivity(sign_up_intent);
-              }
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
+                                              @Override
+                                              public void onClick(View v) {
+                                                  Intent sign_up_intent = new Intent(getApplicationContext(), sign_up.class);
+                                                  startActivity(sign_up_intent);
+                                              }
 
-          }
+                                          }
 
         );
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+            }
+            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+            finish();
+        }  else{
+            Toast.makeText(this, "Login Failed!",Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }

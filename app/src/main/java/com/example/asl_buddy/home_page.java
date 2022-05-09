9bc01d129306ae9496c6571ec72b11ff1c05f481
omamoham <94276035+omamoham@users.noreply.github.com>
2022-05-09 06:14:35 -0500

    signout

--- app/src/main/java/com/example/asl_buddy/home_page.java
+++ app/src/main/java/com/example/asl_buddy/home_page.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class home_page extends AppCompatActivity {  //test
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class home_page extends AppCompatActivity implements View.OnClickListener{  //test
 
     private ImageButton vocabu;
     private Button translate;
+    private Button button_sign_out;
+    GoogleSignInClient mGoogleSignInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,5 +46,34 @@ public class home_page extends AppCompatActivity {  //test
             }
         });
 
+        button_sign_out=findViewById(R.id.button10);
+        button_sign_out.setOnClickListener(this);
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            // ...
+            case R.id.button10:
+                signOut();
+                break;
+            // ...
+        }
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(home_page.this,"Signed Out",Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(home_page.this,MainActivity.class));
+                    }
+                });
+    }
+
+
+
 }
